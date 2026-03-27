00ee9364e1c2259352d69db3d1fc037caede8329
SgnCycles <165931397+SgnCycles@users.noreply.github.com>
2026-03-27 20:00:07 +0100

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,12 +2,12 @@ import styles from './header.module.css'
 import Navigation from '../Navigation'
 import Logo from '../Logo'
 
-const Header = ({updatePage, showMobileMenu, mobileMenu, closeMobileMenu}) => {
+const Header = ({updatePage, showMobileMenu, mobileMenu, closeMobileMenu, currentPage}) => {
 
   return (
     <header className={styles.header}>
       <Logo />
-      <Navigation updatePage={updatePage} showMobileMenu={showMobileMenu} mobileMenu={mobileMenu} closeMobileMenu={closeMobileMenu} />
+      <Navigation updatePage={updatePage} showMobileMenu={showMobileMenu} mobileMenu={mobileMenu} closeMobileMenu={closeMobileMenu} currentPage={currentPage}/>
     </header>
   )
 }
